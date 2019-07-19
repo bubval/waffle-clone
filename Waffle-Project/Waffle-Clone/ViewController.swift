ea96a1baf89ac2c7c1a97ab9343489f7eec512ce
Lyubomir Valkov <lyubo.valkov@gmail.com>
2019-07-19 11:26:23 +0300

    skeleton

---
+++ Waffle-Project/Waffle-Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Waffle-Clone
+//
+//  Created by Lubo on 18.07.19.
+//  Copyright © 2019 Tumba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
