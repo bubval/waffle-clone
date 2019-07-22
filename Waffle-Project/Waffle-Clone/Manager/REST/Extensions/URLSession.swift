e09c5a1e467f7e54fb207724e77f774d7dad24e1
Lubo Valkov <lyubo.valkov@gmail.com>
2019-07-22 18:21:14 +0300

    Completed URLSession extension dealing with Synchronous Data Tasks

--- Waffle-Project/Waffle-Clone/Manager/REST/Extensions/URLSession.swift
+++ Waffle-Project/Waffle-Clone/Manager/REST/Extensions/URLSession.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+import Dispatch
+
+public typealias SynchronousDataTaskResult = (data: Data?, response: URLResponse?, error: Error?)
+
+extension URLSession {
+    
+    public func synchronousDataTask(request: URLRequest) -> SynchronousDataTaskResult {
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let dataTask = self.dataTask(with: request) { (requestData, requestResponse, requestError) in
+            data = requestData
+            response = requestResponse
+            error = requestError
+            semaphore.signal()
+        }
+        dataTask.resume()
+        
+        _ = semaphore.wait(timeout: .distantFuture)
+        
+        return (data, response, error)
+    }
+    
+    public func synchronousDataTask(url: URL) -> SynchronousDataTaskResult {
+        var data: Data?
+        var response: URLResponse?
+        var error: Error?
+        
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let dataTask = self.dataTask(with: url) { (urlData, urlResponse, urlError) in
+            data = urlData
+            response = urlResponse
+            error = urlError
+            semaphore.signal()
+        }
+        dataTask.resume()
+        
+        _ = semaphore.wait(timeout: .distantFuture)
+        
+        return (data, response, error)
+    }
+}
+
