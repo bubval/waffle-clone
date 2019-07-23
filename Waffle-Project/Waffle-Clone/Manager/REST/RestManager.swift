77d90032e506ece490cd1208a03413791dfb6000
Lubo Valkov <lyubo.valkov@gmail.com>
2019-07-23 13:47:39 +0300

    Added GET method with a return, added some comments

--- Waffle-Project/Waffle-Clone/Manager/REST/RestManager.swift
+++ Waffle-Project/Waffle-Clone/Manager/REST/RestManager.swift
@@ -12,6 +12,7 @@ public typealias RestManagerCompletion = (Data?, URLResponse?, Error?) -> Swift.
 public typealias RestManagerResult = SynchronousDataTaskResult
 
 open class RestManager {
+    
     var session: URLSession
     
     public init() {
@@ -22,14 +23,40 @@ open class RestManager {
         self.session = session
     }
     
+    /// Used to retrieve information from a given server. Using GET only retrieves data and has no effect on the data.
+    ///
+    /// - Parameters:
+    ///   - url: API HTTP address
+    ///   - parameters: HTTP request with a query that specifies [name : value] pairs
+    ///   - headers: HTTP request for metadata
+    ///   - completion: Data, URLResponse, Error
     public func get(url: String, parameters: [String : String]? = nil, headers: [String : String]? = nil, completion: @escaping RestManagerCompletion) {
         let request = Request(url: url, method: .GET, parameters: parameters, headers: headers, body: nil)
-        let builtRequest = request.request()
-        if let urlRequest = builtRequest.request {
+        let buildRequest = request.request()
+        if let urlRequest = buildRequest.request {
             let task = session.dataTask(with: urlRequest, completionHandler: completion)
             task.resume()
         } else {
-            completion(nil, nil, builtRequest.error)
+            completion(nil, nil, buildRequest.error)
         }
     }
+    
+    /// Used to retrieve information from a given server. Using GET only retrieves data and has no effect on the data.
+    ///
+    /// - Parameters:
+    ///   - url: API HTTP address
+    ///   - parameters: HTTP request with a query that specifies [name : value] pairs
+    ///   - headers: HTTP request for metadata
+    /// - Returns: Data, URLResponse, Error
+    public func get(url: String, parameters: [String : String]? = nil, headers: [String: String]? = nil) -> RestManagerResult {
+        let request = Request(url: url, method: .GET, parameters: parameters, headers: headers, body: nil)
+        let buildRequest = request.request()
+        if let urlRequest = buildRequest.request {
+            return session.synchronousDataTask(request: urlRequest)
+        } else {
+            return (nil, nil, buildRequest.error)
+        }
+    }
+    
+    
 }
