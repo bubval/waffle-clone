26740c8dd67fc891dfe09b83ca512e39d538f583
Lubo Valkov <lyubo.valkov@gmail.com>
2019-07-29 15:14:24 +0300

    added user model

---
+++ Waffle-Project/Waffle-Clone/Models/User/UserResponse.swift
@@ -0,0 +1,40 @@
+//
+//  UserResponse.swift
+//  Waffle-Clone
+//
+//  Created by Lubo on 29.07.19.
+//  Copyright © 2019 Tumba. All rights reserved.
+//
+
+import Foundation
+
+public struct UserResponse: Codable {
+    let login: String
+    let id: Int
+    let avatarUrl: String
+    let name: String
+    let email: String
+    let bio: String
+    let createdAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case avatarUrl = "avatar_url"
+        case name
+        case email
+        case bio
+        case createdAt = "created_at"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        login = try values.decode(String.self, forKey: .login)
+        id = try values.decode(Int.self, forKey: .id)
+        avatarUrl = try values.decode(String.self, forKey: .avatarUrl)
+        name = try values.decode(String.self, forKey: .name)
+        email = try values.decode(String.self, forKey: .email)
+        bio = try values.decode(String.self, forKey: .bio)
+        createdAt = try values.decode(String.self, forKey: .createdAt)
+    }
+}
